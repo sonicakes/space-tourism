414ab509fa169b4e982a7692b6f58726f1bbbe90
sonicakes <soniaparker1207@gmail.com>
2026-01-14 09:46:45 +1100

    style main CTA

--- app/components/shared/ui/Cta.tsx
+++ app/components/shared/ui/Cta.tsx
@@ -1,6 +1,23 @@
 const Cta = ({ label = "Explore" }: { label?: string }) => {
   return (
-    <div className="bg-white h-30 w-30 rounded-full text-gray-900">{label}</div>
+    <div
+      className="
+        relative
+        cursor-pointer
+        bg-white
+        flex justify-center items-center
+        h-36 w-36 md:h-68 md:w-68
+        rounded-full
+        text-blue-900 font-bellefair-regular
+        text-lg md:text-[2rem] uppercase
+        transition-all duration-300 ease-out
+        hover:text-blue-900/50
+        hover:ring-30 hover:ring-gray-300/30
+        hover:ring-offset-8 hover:ring-offset-transparent
+        "
+    >
+      {label}
+    </div>
   );
 };
 
